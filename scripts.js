46b70861addedc4b88817b1548001572ac171055
Adam Horvath <adam.horvath.0827@gmail.com>
2023-09-20 21:00:16 +0100

    Improved scaling the layout for smaller screens

--- scripts.js
+++ scripts.js
@@ -13,7 +13,7 @@ const sr = ScrollReveal({
     reset: true
 });
 
-sr.reveal('.hero-text', { delay: 200, origin: 'top' });
+sr.reveal('.hero-description', { delay: 200, origin: 'top' });
 sr.reveal('.hero-img', { delay: 450, origin: 'top' });
 sr.reveal('.icons', { delay: 500, origin: 'left' });
 sr.reveal('.scroll-down', { delay: 500, origin: 'right' });
